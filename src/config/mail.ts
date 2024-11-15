971eac9fbf89489527b0a1ae3ccc085dfb3e5b2c
Santiago Loper Marmolejo <santiagolopezmarmolejo2017@gmail.com>
2024-11-15 17:17:16 -0500

    Change env variables and update redis config

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -4,5 +4,7 @@ export const mailBullConfig: BullModuleOptions = {
   redis: {
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
   },
 };
--- src/lib/cause/infrastructure/service/cause.service.ts
+++ src/lib/cause/infrastructure/service/cause.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, RequestTimeoutException } from '@nestjs/common';
+import { ICauseService } from '../../domain/service/ICause.service';
+import { IReason } from 'src/lib/reason/domain/interfaces/IReason';
+import {
+  ICauseCreate,
+  ICause,
+  ICauseUpdate,
+} from '../../domain/interfaces/ICause';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cause } from '../entity/cause.entity';
+import { Repository } from 'typeorm';
+import { Reason } from './../../../reason/infrastructure/entity/reason.entity';
+import { NotFoundError } from './../../../common/domain/errors/NotFoundErrors';
 
 @Injectable()
-export class CauseService {}
+export class CauseService implements ICauseService {
+  constructor(
+    @InjectRepository(Cause)
+    private readonly causeRepository: Repository<Cause>,
+    @InjectRepository(Reason)
+    private readonly reasonRepository: Repository<Reason>,
+  ) {}
+
+  async create(id: IReason['id'], cause: ICauseCreate): Promise<ICause> {
+    let reason: IReason | undefined;
+    let newCause: ICause | undefined;
+    try {
+      reason = await this.reasonRepository.findOneBy({ id });
+    } catch (error) {
+      throw new NotFoundError('Reason not found');
+    }
+
+    try {
+      newCause = this.causeRepository.create(cause);
+    } catch (error) {
+      throw new RequestTimeoutException('Could not create cause');
+    }
+
+    try {
+      reason.causes.push(newCause);
+      await this.reasonRepository.save(reason);
+    } catch (error) {
+      throw new RequestTimeoutException('Could not save cause');
+    }
+    return newCause;
+  }
+
+  async update(id: ICause['id'], cause: ICauseUpdate): Promise<boolean> {
+    let causeToUpdate: ICause | undefined;
+    try {
+      causeToUpdate = await this.causeRepository.findOneBy({ id });
+    } catch (error) {
+      throw new NotFoundError('Cause not found');
+    }
+
+    causeToUpdate.name = cause.name ?? causeToUpdate.name;
+    try {
+      await this.causeRepository.save(causeToUpdate);
+    } catch (error) {
+      throw new RequestTimeoutException('Could not update cause');
+    }
+    return true;
+  }
+
+  async get(id: IReason['id']): Promise<ICause[]> {
+    return await this.causeRepository.find({
+      where: {
+        reason: {
+          id: id,
+        },
+      },
+    });
+  }
+}
