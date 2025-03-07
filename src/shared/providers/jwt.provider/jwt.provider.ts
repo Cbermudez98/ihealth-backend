bb0959ab6a2799ca849d44eb1104943ceeaac63e
Cesar Bermudez <cesar.bermudez.sierra@gmail.com>
2025-03-07 16:45:12 -0500

    Remove JwtModule

--- src/shared/providers/jwt.provider/jwt.provider.ts
+++ src/shared/providers/jwt.provider/jwt.provider.ts
@@ -1,14 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { IJwtService } from 'src/lib/auth/domain/service/IJwt.service';
 
 @Injectable()
 export class JwtProvider implements IJwtService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly config: ConfigService,
+  ) {}
 
   signToken(payload: Record<string, any>): string {
     return this.jwtService.sign(payload, {
-      secret: '123123123123',
+      secret: this.config.get<string>('JWT_SECRET_KEY'),
     });
   }
 
